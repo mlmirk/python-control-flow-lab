ef237ca26dd779252e5749a9ae5ab653a0e1bf23
mlmirk <mike.l.mirkovic@gmail.com>
2021-11-16 08:36:27 -0500

    5 and 6 complete still want to work on error catching and fibbo

--- exercise-5.py
+++ exercise-5.py
@@ -12,3 +12,16 @@
 #      etc.
 
 # Hint: The next number is found by adding the two numbers before it
+def fibonacci (n):
+  a = 0
+  b = 1
+  if n >= 0:
+    print("term: 0 / number: 0")
+    print("term: 1 / number: 1")
+    for i in range(0, n):
+      c = a + b
+      a = b
+      b = c
+      print(f"term: {i} / number: {c}")
+
+fibonacci(51)
--- exercise-6.py
+++ exercise-6.py
@@ -19,3 +19,33 @@
 #
 # After setting the likely season, you can use another if...elif...else statement to "adjust" if
 # the day number falls within a certain range.
+winter=('jan','feb','mar')
+spring=('apr','may','jun')
+summer=('jul','aug','sep')
+fall=('oct','nov','dec')
+
+user_input_month = input("Enter a month in a three letter abbreviation[jan,feb,mar] ").lower()
+user_input_day = int(input("enter a day in the month "))
+day=user_input_day
+if user_input_month in winter:
+  if user_input_month == 'mar' and user_input_day>19:
+    print(f'{user_input_month} {user_input_day} is in spring ')
+  else:
+    print(f'{user_input_month} {user_input_day} is in winter ')
+elif user_input_month in spring:
+  if user_input_month == 'jun' and user_input_day>20:
+    print(f'{user_input_month} {user_input_day} is in summer ')
+  else:
+    print(f'{user_input_month} {user_input_day} is in spring ')
+elif user_input_month in summer:
+  if user_input_month == 'sep' and user_input_day>21:
+    print(f'{user_input_month} {user_input_day} is in fall ')
+  else:
+    print(f'{user_input_month} {user_input_day} is in summer ')
+elif user_input_month in fall:
+  if user_input_month == 'dec' and user_input_day>20:
+    print(f'{user_input_month} {user_input_day} is in winter ')
+  else:
+    print(f'{user_input_month} {user_input_day} is in fall ')
+else:
+  print(f'I dont think {user_input_month} and {user_input_day} followed the correct format')
